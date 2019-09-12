990e52d6478b1be42cf6d330bcdc2e68af65321a
niraj1018 <niraj1018@gmail.com>
2019-09-11 21:05:54 -0400

    Update Main.java

--- SBA_Nirajv2_Java_Junit/Java/Area of Shapes/src/Main.java
+++ SBA_Nirajv2_Java_Junit/Java/Area of Shapes/src/Main.java
@@ -1,26 +1,32 @@
-class Shapes
-{
-	void area()
-	{
-		System.out.println("Displaying Area of different shapes");
-	}
-}
-
-// Fill your code to display the formula for area of Circle and Square
-class Circle		//Make other changes but DO NOT change name of the class
-{
-
-}
-
-class Square		//Make other changes but DO NOT change name of the class
-{
-
-}
+import java.util.Scanner;
 
 public class Main {
 	public static void main(String args[])
 	{
 		//Call the method to print the formula for area of respective shapes using Runtime Polymorphism
+		String shape;
+		System.out.println("Please enter the shape for which you need the formula for area");
+		
+		
+		// TAKE INPUTS DURING RUNTIME
+		Scanner sc = new Scanner (System.in);
+		shape = sc.next();
+		
+
+		// Area of circle is pi*side*side
+
+if (shape.equalsIgnoreCase("circle"))
+		{
+			Shapes sp = new Circle();
+			sp.area();
+		}
+
+// Area of square is side*side
+	if (shape.equalsIgnoreCase("square"))
+		{
+			Shapes sp = new Square();
+			sp.area();
+		}
 	}
 	
 }
